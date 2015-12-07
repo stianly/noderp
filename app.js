3e6a934fbb541cec8214123a37d831058ed6c6b4
stianly <stianly@hotmail.com>
2015-12-07 17:15:00 +0100

    html for jade and index/css setup

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var gulp = require('gulp');
+var swig = require('swig');
+var cons = require('consolidate');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,25 +14,30 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+
+swig.setDefaults({ varControls: ['<%=', '%>'] }); 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.engine('html', cons.swig);
+    app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+
+app.get('/', function(req, res) {
+    res.render('index.html');
 });
 
 // error handlers
